0e266f6ff0e735c3362d33316022aba4755967b9
Theo Brown <tab53@cam.ac.uk>
2017-07-03 15:22:10 +0100

    Added calculation and display of FFT and sonogram (built in functions)

--- sonogram.py
+++ sonogram.py
@@ -7,13 +7,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def source_function(t, f=100, x=5):
-    return np.exp((1j * f*2*np.pi - x) * t)
+sample_freq = 200
 
 
-t = np.arange(0, 1, 0.0001)  # Create time range
-y = source_function(t)      # Create data
+def source_function(t, x=10):
+    return np.exp((1j * sample_freq*2*np.pi - x) * t)
 
+
+def to_db(x):
+    return 20*np.log10(x)
+
+
+# Create time range
+t = np.arange(0, 2.5, 0.0001)
+# Create data
+y = source_function(t)
+
+# Plot data
 plt.plot(t, y)
+# plt.show()
+
+# Take FFT of data
+sp = np.fft.fft(y)
+
+# Plot FFT
+plt.magnitude_spectrum(y, Fs=sample_freq, scale='dB')
 plt.show()
 
+# Plot spectrogram
+plt.specgram(y, Fs=sample_freq)
+plt.show()
