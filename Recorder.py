29dfcdaa3c6c03b5e18607243526e768a173f5e5
Howard T <howard.tee.en.yi@gmail.com>
2017-07-03 20:17:48 +0100

    Add recoder class and a test file

---
+++ Recorder.py
@@ -0,0 +1,120 @@
+import pyaudio
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+Recorder class:
+- Allow recording and configurations of recording device
+- Return recording as numpy array
+"""
+
+class Recorder():
+    def __init__(self,channels,rate,frames_per_buffer,audio_format):
+        self.channels = channels
+        self.rate = rate
+        self.frames_per_buffer = frames_per_buffer
+        self.format = audio_format
+        self.p = pyaudio.PyAudio()
+        self.filename = None
+        
+    # TODO: Define function to initialise a PyAudio object
+    
+    # Originally setting file name for the wave file    
+    def set_filename(self,filename):
+        self.filename = filename
+        
+    '''def play_recording(self,filename = None):
+        if filename == None:
+            filename = self.filename
+        
+        if filename != None:
+            wf = wave.open(filename,'rb')
+
+            output_stream = self.p.open(format = self.p.get_format_from_width(wf.getsampwidth()),
+                            channels = wf.getnchannels(),
+                            rate = wf.getframerate(),
+                            output = True)
+            
+            data = wf.readframes(self.frames_per_buffer)
+
+            while len(data)>0:
+                output_stream.write(data)
+                data = wf.readframes(self.frames_per_buffer)
+
+            wf.close()
+            output_stream.stop_stream()
+            output_stream.close()'''
+
+    # Get recording device info 
+    def device_info_name(self):
+        for i in range(self.p.get_device_count()):
+            print("%i) %s" % (i,self.p.get_device_info_by_index(i)['name']))
+            
+    #TODO: Add function(s) to configure recording device
+            # Like changing recording device
+            
+    # May want to change to a callback method for responsive recording
+    # Currently it is using blocking method
+    def record(self,duration):
+        '''rc = wave.open(self.filename,'wb')
+        rc.setnchannels(self.channels)
+        rc.setsampwidth(self.p.get_sample_size(pyaudio.paInt16))
+        rc.setframerate(self.rate)'''
+        data_array = []
+        input_stream = self.p.open(channels = self.channels,
+                         rate = self.rate,
+                         format = self.format,
+                         input = True,
+                         frames_per_buffer = self.frames_per_buffer)
+
+        print('RECORDING...')
+        for _ in range(int(self.rate/self.frames_per_buffer * duration)):
+            data = input_stream.read(self.frames_per_buffer)
+            #rc.writeframes(data)
+            data_array.append(np.fromstring(data,dtype = np.int16))
+
+        print('RECORDING END')
+            #rc.close()
+        input_stream.stop_stream()
+        input_stream.close()
+
+        return np.hstack(data_array)
+
+    #TODO: Complete callback function for non-blocking method
+    def record_callback(self,in_data,frame_count,time_info,status_flag):
+        #TODO: insert code to record data
+        return(data,pyaudio.paContinue)
+    
+    #TODO: Live oscilloscope here?
+    def show_recording(self, duration):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        
+        input_stream = self.p.open(channels = self.channels,
+                         rate = self.rate,
+                         format = self.format,
+                         input = True,
+                         frames_per_buffer = self.frames_per_buffer)
+        print('LISTENING...')
+        data = np.array(np.fromstring( input_stream.read(self.frames_per_buffer),
+                              dtype = np.int16))
+        
+        line = ax.plot(np.array(range(len(data)))/self.rate,data)[0]
+        ax.set_ylim(-10000,10000)
+        fig.show()
+        for _ in range(int(self.rate/self.frames_per_buffer * duration)):
+            data = np.array(np.fromstring( input_stream.read(self.frames_per_buffer),
+                              dtype = np.int16))
+            line.set_ydata(data)
+            fig.canvas.draw()
+
+        print('LISTENING END')
+        plt.close(fig)
+        input_stream.stop_stream()
+        input_stream.close()
+
+        
+    def close(self):
+        self.p.terminate()
+
