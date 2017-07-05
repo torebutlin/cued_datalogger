7d5602b3893ac5c31a85ae4aba68ea47db9a54fa
Theo Brown <tab53@cam.ac.uk>
2017-07-05 12:47:41 +0100

    Added comments, labels to graphs, general tidy-up

--- sonogram.py
+++ sonogram.py
@@ -1,6 +1,7 @@
 """
 Sonogram
 Author: Theo Brown (tab53)
+Date: July 2017
 """
 
 from numpy import fft
@@ -13,34 +14,29 @@ from scipy import signal
 
 # Function source
 def func_1(t, w, x):
+    """
+    A simple function (decaying exponential)
+    """
     return np.exp((1j*w - x) * t)
 
-def func_2(t, w):
-    return np.sin(w*t)
 
 def function_generator(t):
     """
-    f1 = func_1(t, 50*2*np.pi, 5)
-    f2 = func_1(t, 80*2*np.pi, 3)
-    f1_echo = f1/3
-    f2_echo = f2/4
-    result = f1 + f2
-    echo = f1_echo[:f1_echo.size/2] + f2_echo[:f2_echo.size/2]
-    result[result.size/2:] += echo
+    Creates a test function with echoes
     """
-    f1 = func_2(t, 50*2*np.pi)
-    f2 = func_2(t, 80*2*np.pi)
-    result = f1 + 0.5*f2
-    result[result.size/4:result.size/2] *= 0
-    result[result.size*3/4:] *= 0
+    f1 = func_1(t, 50*2*np.pi, 5)
+    f2 = func_1(t, 243*2*np.pi, 3)
+    result = f1 + f2
+    echo1 = 0.5*(f1[:f1.size/2] + f2[:f2.size/2])
+    result[result.size/2:] += echo1
+    echo2 = 0.3*(f1[:f1.size/4] + f2[:f2.size/4])
+    result[result.size*3/4:] += echo2
     return result
 
-def dB(x):
-    return 20*np.log10(x)
 
 # Sampling information:
 # Sampling frequency
-f_s = 44.1e3
+f_s = 4096
 # Sampling interval
 dt_s = 1/f_s
 
@@ -48,9 +44,9 @@ dt_s = 1/f_s
 # Length of time series
 len_t = 2
 # Number of points
-#N_t = 4e3
+# N_t = 4e3
 # Interval between points
-dt_t = 1e-3
+dt_t = 1e-4
 # Create time vector
 t = np.arange(0.0, len_t, dt_t)
 # Create data
@@ -60,6 +56,7 @@ y = function_generator(t)
 fig_y = plt.figure()
 plt.plot(t, y)
 
+"""
 ## Fourier Transform
 # Take FFT of data
 sp = fft.fft(y)
@@ -70,35 +67,37 @@ sp = sp[:sp.size/2]
 f = f[:f.size/2]
 
 # Plot FFT
-
 fig_sp = plt.figure()
 plt.plot(f, np.abs(sp))
 plt.xlabel('Freq (Hz)')
 plt.ylabel('Amplitude')
-
+"""
 
 ##################
 # SONOGRAM       #
 ##################
 # Set up variables for window of fft
 width_W = 256
-step_W = 32
+step_W = 4
 N_W = int(y.size/step_W)
 
 # Create data structure to store spectrogram in
+t = np.linspace(0, len_t, N_W)
 sp = np.zeros((t.size, width_W))
-f = fft.fftfreq(width_W, dt_s) * dt_s * 1e3
+f = fft.fftfreq(width_W, dt_s) * dt_s / dt_t
 
-i = 0
+# Set window properties
 lower_W = 0
 upper_W = lower_W + width_W
+
+i = 0
 
 # Take FFT of sliding window
 # Repeat until number of spectra reached
 while i < N_W:
     print("Taking fft of window {}".format(i))
     # Take FFT of window
-    sp[lower_W] = fft.fft(y[lower_W:upper_W], n=sp[lower_W].size)
+    sp[i] = fft.fft(y[lower_W:upper_W], n=sp[i].size)
     # Next spectrum
     i += 1
     # Increment lower bound of window
@@ -114,26 +113,32 @@ f = f[:len(f)/2]
 # Create grid of data points
 F, T = np.meshgrid(f, t)
 
+# Find magnitude of freqency components
 sp = np.abs(sp)
 
 # Plot sonogram
+# Contours:
 fig_sonogram_cont = plt.figure()
 contours = plt.contour(F, T, sp)
+plt.xlabel('Freq (Hz)')
+plt.xlim(0, 1000)
+plt.ylabel('Time (s)')
 
+# Surface:
 fig_sonogram_surf = plt.figure()
 ax = fig_sonogram_surf.gca(projection='3d')
 surf = ax.plot_surface(F, T, sp, linewidth=0, cmap=cm.coolwarm)
-"""
-fig_sonogram_color = plt.figure()
-plt.pcolormesh(t, f, sp)
-"""
+ax.set_xlabel('Freq (Hz)')
+ax.set_xlim(0, 1000)
+ax.set_ylabel('Time (s)')
+ax.set_zlabel('Amplitude')
 
-# Built in functions:
-fig_spectrogram = plt.figure()
-f, t, Sxx = signal.spectrogram(y, f_s)
-plt.pcolormesh(t, f, Sxx)
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
+# Colourmap:
+fig_sonogram_color = plt.figure()
+plt.pcolormesh(F, T, sp)
+plt.xlabel('Freq (Hz)')
+plt.xlim(0, 1000)
+plt.ylabel('Time (s)')
 
 # Display graphs
 plt.show()
