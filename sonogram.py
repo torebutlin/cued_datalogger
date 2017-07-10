395312a096365dd08381f54581dae80d5dd4bfff
Theo Brown <tab53@cam.ac.uk>
2017-07-10 19:02:03 +0100

    Put sonogram-redo in functions and tidied things up

--- sonogram.py
+++ sonogram.py
@@ -4,204 +4,95 @@ Author: Theo Brown (tab53)
 Date: July 2017
 """
 
-from numpy import fft
+from numpy.fft import rfft, rfftfreq
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from time import sleep
 
-# Function source
-def func_1(t, w, x):
+SAMPLE_FREQ = 4096
+WINDOW_WIDTH = 256
+WINDOW_INCREMENT = 32
+
+
+def func_1(t, w, x, k=0):
     """
-    A simple function (decaying exponential)
+    A simple decaying sine wave function
     """
-    return np.exp((1j*w - x) * t)
+    return np.exp((1j*w - x)*t)
 
 
 def function_generator(t):
     """
-    Creates a test function
+    A function with echoes
     """
-    """
-    f1 = func_1(t, 1000*2*np.pi, 5)
-    #f2 = func_1(t, 4000*2*np.pi, 3)
-    result = f1# + f2
-    echo1 = f1[:int(f1.size/2)] #+ f2[:int(f2.size/2)]
-    result[int(result.size/2):] += echo1
-    #echo2 = 0.3*(f1[:int(f1.size/4)] + f2[:int(f2.size/4)])
-    #result[int(result.size*3/4):] += echo2
-    """
-    result = np.sin(1000*2*np.pi*t)
-    #result[:int(len(t)/4)] *= 0
-    #result[int(len(t)*3/4):] *= 0
+    f1 = func_1(t, 2000*2*np.pi, 2)
+    f2 = func_1(t, 500*2*np.pi, 1)
+    # Create an echo of one of the functions
+    f1[np.floor(f1.size/2):] += f1[:np.floor(f1.size/2)]
+    result = f1 + f2
     return result
 
 
-def fft_of_window(signal, window_lower, window_upper, window_shape):
+def window_fft(signal, window_lower, window_upper, window_shape):
     """
-    Takes the Fourier Transform of a window of values in a signal
+    Takes the FT of a window within a signal, scaled by a shape array
     """
-    window_width = window_upper - window_lower
-    # Scale window by window shape
-    window_values = np.multiply(signal[window_lower:window_upper], window_shape)
-    # Take FFT of scaled window
-    return fft.rfft(window_values, n=window_width)
+    window = np.multiply(signal[window_lower:window_upper], window_shape)
+
+    return np.abs(rfft(window))
 
 
-def sliding_window_fft(t, signal, fft_sample_freq, window_width=256, window_increment=32, window_shape='hanning'):
+def sliding_window_fft(signal, t, window_shape):
     """
-    Takes successive Fourier Transforms of a window as it is slid across a 1D signal
+    Takes the FT of a shaped window as it is incremented across a signal
     """
-    ## Extend signal so that first time bin for FFT can be taken at t=0
-    # This extends the signal so it has width/2 zeros before the signal and the same after the signal
-    signal_extended = np.append(np.zeros(int(window_width/2)), signal)
-    signal_extended = np.append(signal_extended, np.zeros(int(window_width/2)))
+    num_windows = np.floor(signal.size / WINDOW_INCREMENT)
 
-    ## Set up window
-    # Default: Hanning
-    if window_shape == 'hanning':
-        window_shape = np.hanning(window_width)
-    else:
-        raise ValueError("Unrecognised window shape.")
-    window_lower = 0
-    window_upper = window_lower + window_width
+    # Create the array to store the FT results in
+    FT = np.zeros((num_windows, WINDOW_WIDTH/2 + 1))
 
-    ## Set up FT
-    FT = np.zeros((int(signal.size / window_width), int(window_width/2) + 1))
+    # Find the associated time vector and frequencies for the FT
+    times = np.linspace(0, t[-1], num=num_windows)
+    freqs = rfftfreq(WINDOW_WIDTH, 1/SAMPLE_FREQ)
 
-    ## Find associated frequencies of the FT
-    freqs = fft.rfftfreq(window_width, 1./fft_sample_freq)
+    # Extend the signal so that the window can be slid off the ends
+    signal_extended = np.append(np.zeros(int(WINDOW_WIDTH/2)), signal)
+    signal_extended = np.append(signal_extended, np.zeros(int(WINDOW_WIDTH/2)))
 
-    ## Find associated time bins of the FT
-    FT_t = np.linspace(0, t[-1], num=FT.shape[0])
+    # Iterate through the signal taking FTs of each window
+    for i in np.arange(num_windows):
+        lower = i * WINDOW_INCREMENT
+        upper = lower + WINDOW_WIDTH
 
-    ## Slide the window along and take FFTs
-    for i in np.arange(FT.shape[0]):
-        # Take FFT of window
-        FT[i] = fft_of_window(signal_extended, window_lower, window_upper, window_shape)
-        # Increment window
-        window_lower += window_increment
-        window_upper += window_increment
-        i+=1
+        FT[i] = window_fft(signal_extended, lower, upper, window_shape)
 
-    return freqs, FT_t, FT
+    return freqs, times, FT
 
 
-# Define sampling frequency
-sample_freq = 4096
-## Create time series
-# Length of time series
-len_t = 10
-# Interval between points
-dt_t = 1/sample_freq
-# Create time vector
-t = np.arange(0.0, len_t, dt_t)
-# Create data
-y = function_generator(t)
+def sonogram(signal, t):
+    """
+    Plots a sonogram (frequencies present over time) of a signal
+    """
+    # Create the window
+    hanning = np.hanning(WINDOW_WIDTH)
+
+    # Take the FTs
+    freqs, times, FT = sliding_window_fft(signal, t, hanning)
+
+    # Convert the time and freq to a mesh (for plotting in 3d)
+    F_bins, T_bins = np.meshgrid(freqs, times)
+
+    sonogram = plt.figure("Sonogram")
+    plt.contour(F_bins, T_bins, FT)
+    
+    return sonogram
+
+# Create the input signal
+duration = 10.0
+t = np.arange(0.0, duration, 1/SAMPLE_FREQ)
+signal = function_generator(t)
 plt.figure()
-plt.plot(t, y)
+plt.plot(t, signal)
 
-##################
-# SONOGRAM       #
-##################
-## Calculate sonogram
-freqs, FT_t, FT = sliding_window_fft(t, y, fft_sample_freq=sample_freq, window_increment=32)
+sonogram(signal, t)
 
-# Create grid of data points
-FREQS, FT_T = np.meshgrid(freqs, FT_t)
-
-# Find magnitude of freqency components
-FT = np.abs(FT)
-
-"""
-Test
-"""
-"""
-fig = plt.figure()
-plt.ion()
 plt.show()
-ax = fig.add_subplot(111)
-line1, = ax.plot(freqs, FT[0]) # Returns a tuple of line objects, thus the comma
-i=0
-while i < len(FT):
-    print("Window: " + str(i))
-    line1.set_ydata(FT[i])
-    plt.draw()
-    plt.pause(0.01)
-    i+=1
-"""
-# Plot sonogram
-# Contours:
-"""
-fig_sonogram_cont = plt.figure()
-contours = plt.contour(FREQS, FT_T, FT)
-plt.xlabel('Freq (Hz)')
-#plt.xlim(800, 1200)
-#plt.xlim(300,700)
-#plt.ylim(1,1.25)
-plt.ylabel('Time (s)')
-
-
-# Surface:
-
-fig_sonogram_surf = plt.figure()
-ax_s = fig_sonogram_surf.gca(projection='3d')
-surf = ax_s.plot_surface(FREQS, FT_T, FT, linewidth=0, cmap=cm.jet)
-ax_s.set_xlabel('Freq (Hz)')
-#ax_s.set_xlim(500, 1500)
-ax_s.set_ylabel('Time (s)')
-ax_s.set_zlabel('Amplitude')
-#"""
-"""
-# Colourmap:
-fig_sonogram_color = plt.figure()
-plt.pcolormesh(FREQS, FT_T, FT)
-plt.xlabel('Freq (Hz)')
-plt.xlim(500, 1500)
-plt.ylabel('Time (s)')
-
-# Window 221
-# This is an example of a window that didn't work in the FFT window-sliding loop (sp = 0) - yet when recalculated worked fine
-fig_221_sig = plt.figure()
-plt.suptitle("Window 221: Signal")
-plt.plot(np.arange(0,256), window_221)
-plt.legend(['Signal'])
-
-fig_221_freq = plt.figure()
-plt.suptitle("Window 221: Frequency")
-plt.plot(f, np.abs(sp_221))
-
-sp_221_calc = fft.rfft(window_221, n=width_W)
-plt.plot(f, np.abs(sp_221_calc))
-plt.xlim(0)
-plt.legend(['Spectrum', 'Recalculated Spectrum'])
-fact = np.abs(sp_221_calc)/np.abs(sp_221)
-"""
-
-# 63 and 62 correspond to 992 and 1008 Hz
-fig = plt.figure()
-ax1 = fig.add_subplot(221)
-line1, = ax1.plot(FT_t, FT[:, 62])
-line2, = ax1.plot(FT_t, FT[:, 63])
-ax2 = fig.add_subplot(222)
-line3 = ax2.contour(FREQS, FT_T, FT, linewidth=0, cmap=cm.jet)
-
-i = 10
-while i < 20:
-    fs = 2 ** i
-    print("Freq: " + str(fs))
-    freqs, FT_t, FT = sliding_window_fft(t, y, fft_sample_freq=fs, window_increment=32)
-    FREQS, FT_T = np.meshgrid(freqs, FT_t)
-    fig = plt.figure()
-    ax1 = fig.add_subplot(221)
-    line1, = ax1.plot(FT_t, FT[:, 62])
-    line2, = ax1.plot(FT_t, FT[:, 63])
-    ax2 = fig.add_subplot(222)
-    line3 = ax2.contour(FREQS, FT_T, FT, linewidth=0, cmap=cm.jet)
-    plt.show()
-    i+=1
-
-# Display graphs
-#plt.show()
-
