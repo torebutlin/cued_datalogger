3b13255747abcb9a219feef644d2fb72b483f8dc
Theo Brown <tab53@cam.ac.uk>
2017-07-13 17:22:07 +0100

    Implement stepped slider and spinbox

--- matplotlib/sonogram-mpl.py
+++ matplotlib/sonogram-mpl.py
@@ -3,7 +3,7 @@ import sys
 from PyQt5 import QtCore
 from PyQt5.QtCore import QObject, Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QSlider, QLabel, QSpinBox, QHBoxLayout, QGridLayout, QComboBox
-
+from mypyqt_widgets import Power2SteppedSlider, Power2SteppedSpinBox
 from pyqt_matplotlib import MatplotlibCanvas
 
 import numpy as np
@@ -62,13 +62,13 @@ class SonogramPlotMPL(MatplotlibCanvas):
     def update_plot(self, value):
         sender_name = self.sender().objectName()
         
-        if sender_name == "window_width_spinbox":
+        if sender_name == "window_width_spinbox" or sender_name == "window_width_slider":
             self.window_width = value
-            
-        elif sender_name == "window_increment_spinbox":
+           
+        elif sender_name == "window_increment_spinbox" or sender_name == "window_increment_slider":
             self.window_increment = value
             
-        elif sender_name == "sample_freq_spinbox":
+        elif sender_name == "sample_freq_spinbox" or sender_name == "sample_freq_slider":
             self.sample_freq = value
         
         elif sender_name == "plot_type_combobox":
@@ -94,59 +94,77 @@ class SonogramWidget(QWidget):
     def init_ui(self):
         self.sonogram_plot_mpl = SonogramPlotMPL(self.sig, self.t, self.sample_freq, self.window_width, self.window_increment)
 
-        # Window width interactivity
+        ## Window width interactivity
         self.window_width_label = QLabel(self)
         self.window_width_label.setText("Window width")
-        
-        self.window_width_spinbox = QSpinBox(self)
+        # Create spinbox
+        self.window_width_spinbox = Power2SteppedSpinBox(self)
         self.window_width_spinbox.setObjectName("window_width_spinbox")
-        self.window_width_spinbox.setRange(2, 512)
-        self.window_width_spinbox.setValue(self.window_width)
-        self.window_width_spinbox.setSingleStep(32)
-
-        self.window_width_slider = QSlider(Qt.Horizontal, self)
+        self.window_width_spinbox.setRange(16, 512)
+        # Create slider
+        self.window_width_slider = Power2SteppedSlider(Qt.Horizontal, self)
         self.window_width_slider.setObjectName("window_width_slider")
-        self.window_width_slider.setRange(2, 512)
-        self.window_width_slider.setSingleStep(32)
-        self.window_width_slider.setValue(self.window_width)
-        
-        self.window_width_slider.valueChanged.connect(self.window_width_spinbox.setValue)
+        self.window_width_slider.setRange(16, 512)
+        # Connect spinbox and slider together
         self.window_width_spinbox.valueChanged.connect(self.window_width_slider.setValue)
-
+        self.window_width_slider.valueChanged.connect(self.window_width_spinbox.setValue)
+        # Set values
+        self.window_width_spinbox.setValue(self.window_width)
+        self.window_width_slider.setValue(self.window_width)
+        # Update screen on change
+        self.window_width_slider.valueChanged.connect(self.sonogram_plot_mpl.update_plot)
         self.window_width_spinbox.valueChanged.connect(self.sonogram_plot_mpl.update_plot)
         
-        # Window increment interativity
+        ## Window increment interactivity
         self.window_increment_label = QLabel(self)
         self.window_increment_label.setText("Window increment")
-        
-        self.window_increment_spinbox = QSpinBox(self)
+        # Create spinbox       
+        self.window_increment_spinbox = Power2SteppedSpinBox(self)
         self.window_increment_spinbox.setObjectName("window_increment_spinbox")        
-        self.window_increment_spinbox.setRange(2, 128)
+        self.window_increment_spinbox.setRange(2, 256)
+        # Create slider        
+        self.window_increment_slider = Power2SteppedSlider(Qt.Horizontal, self)
+        self.window_increment_slider.setObjectName("window_increment_slider")
+        self.window_increment_slider.setRange(16, 512)
+        # Connect spinbox and slider together
+        self.window_increment_spinbox.valueChanged.connect(self.window_increment_slider.setValue)
+        self.window_increment_slider.valueChanged.connect(self.window_increment_spinbox.setValue)
+        # Set values
         self.window_increment_spinbox.setValue(self.window_increment)
-        self.window_increment_spinbox.setSingleStep(32)
-        
+        self.window_increment_slider.setValue(self.window_increment)
+        # Update screen on change
+        self.window_increment_slider.valueChanged.connect(self.sonogram_plot_mpl.update_plot)
         self.window_increment_spinbox.valueChanged.connect(self.sonogram_plot_mpl.update_plot)
         
-        # Sample frequency interactivity
+        ## Sample freq interactivity
         self.sample_freq_label = QLabel(self)
-        self.sample_freq_label.setText("Sample frequency")
-        
-        self.sample_freq_spinbox = QSpinBox(self)
+        self.sample_freq_label.setText("Sample freq")
+        # Create spinbox       
+        self.sample_freq_spinbox = Power2SteppedSpinBox(self)
         self.sample_freq_spinbox.setObjectName("sample_freq_spinbox")        
-        self.sample_freq_spinbox.setRange(2, 44.1e3)
+        self.sample_freq_spinbox.setRange(2, 256)
+        # Create slider        
+        self.sample_freq_slider = Power2SteppedSlider(Qt.Horizontal, self)
+        self.sample_freq_slider.setObjectName("sample_freq_slider")
+        self.sample_freq_slider.setRange(16, 512)
+        # Connect spinbox and slider together
+        self.sample_freq_spinbox.valueChanged.connect(self.sample_freq_slider.setValue)
+        self.sample_freq_slider.valueChanged.connect(self.sample_freq_spinbox.setValue)
+        # Set values
         self.sample_freq_spinbox.setValue(self.sample_freq)
-        self.sample_freq_spinbox.setSingleStep(32)
-        
+        self.sample_freq_slider.setValue(self.sample_freq)
+        # Update screen on change
+        self.sample_freq_slider.valueChanged.connect(self.sonogram_plot_mpl.update_plot)
         self.sample_freq_spinbox.valueChanged.connect(self.sonogram_plot_mpl.update_plot)
         
-        # Plot type interactivity
+        ## Plot type interactivity
         self.plot_type_label = QLabel(self)
         self.plot_type_label.setText("Plot type")
-        
+        # Create combobox
         self.plot_type_combobox = QComboBox(self)
         self.plot_type_combobox.addItems(["Contour", "Surface", "Colourmap"])
         self.plot_type_combobox.setObjectName("plot_type_combobox")
-        
+        # Update on change
         self.plot_type_combobox.activated[str].connect(self.sonogram_plot_mpl.update_plot)        
         
         # Layout
@@ -156,8 +174,10 @@ class SonogramWidget(QWidget):
         grid.addWidget(self.window_width_slider, 0, 2)
         grid.addWidget(self.window_increment_label, 1, 0)
         grid.addWidget(self.window_increment_spinbox, 1, 1)
+        grid.addWidget(self.window_increment_slider, 1, 2)
         grid.addWidget(self.sample_freq_label, 2, 0)
         grid.addWidget(self.sample_freq_spinbox, 2, 1)
+        grid.addWidget(self.sample_freq_slider, 2, 2)
         grid.addWidget(self.plot_type_label, 3, 0)
         grid.addWidget(self.plot_type_combobox, 3, 1)
         
