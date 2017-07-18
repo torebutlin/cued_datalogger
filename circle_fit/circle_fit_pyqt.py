62f310cf72a7582033a29147dbb3bdd2bc3830ee
Theo Brown <tab53@cam.ac.uk>
2017-07-18 17:10:44 +0100

    Improved region zoom

--- circle_fit/circle_fit_pyqt.py
+++ circle_fit/circle_fit_pyqt.py
@@ -45,14 +45,13 @@ def circle_plot(x0, y0, R0):
     return x, y
 
 def update_plots():
-    wmin, wmax = lr.getRegion()
+    wmin, wmax = p1.getAxis('bottom').range
+    #dmin, dmax = p1.getAxis('left').range
     
     lower = np.where(w>wmin)[0][0]
     upper = np.where(w<wmax)[0][-1]
-    print(lower, upper)
-
+    
     d_ = d[lower:upper]
-    #d_=d
     
     p2.clear()
     p2.plot(d_.real, d_.imag)
@@ -64,12 +63,12 @@ def update_plots():
     p3.plot(x_c, y_c, pen=pg.mkPen('r', width=2, style=QtCore.Qt.DashLine))
 
 
-from pyqtgraph.Qt import QtGui, QtCore
+from pyqtgraph.Qt import QtCore
 import pyqtgraph as pg
 
-# Calculate FRF
+# Create a demo FRF
 w = np.linspace(0, 25 ,1e5)
-d = func(w, 10, 1e-2, 500j) + func(w, 5, 1e-2, 200j) 
+d = 20*np.log10(func(w, 10, 1e-2, 8e12+1e9j) + func(w, 5, 1e-1, 8e12+1e9j) + func(w, 20, 5e-2, 8e12+1e9j) + func(w, 12, 2e-2, 8e12+1e9j))
 
 
 win = pg.GraphicsWindow(title="Circle Fit")
@@ -77,17 +76,26 @@ win = pg.GraphicsWindow(title="Circle Fit")
 # Transfer fn
 p1 = win.addPlot(title="Transfer function", x=w, y=np.abs(d))
 p_select = win.addPlot(title="Region Selection", x=w, y=np.abs(d))
-lr = pg.LinearRegionItem([w.min(), w.max()])
-lr.setZValue(-10)
-p_select.addItem(lr)
+lr_x = pg.LinearRegionItem([w.min(), w.max()], orientation=pg.LinearRegionItem.Vertical)
+lr_y = pg.LinearRegionItem([d.min(), d.max()], orientation=pg.LinearRegionItem.Horizontal)
+lr_x.setZValue(-10)
+lr_y.setZValue(-10)
+p_select.addItem(lr_x)
+p_select.addItem(lr_y)
+
 
 def update_zoom():
-    p1.setXRange(*lr.getRegion(), padding=0)
+    p1.setXRange(*lr_x.getRegion(), padding=0)
+    p1.setYRange(*lr_y.getRegion(), padding=0)
+
 def update_region():
-    lr.setRegion(p1.getViewBox().viewRange()[0])
+    lr_x.setRegion(p1.getViewBox().viewRange()[0])
+    lr_y.setRegion(p1.getViewBox().viewRange()[1])
     
-lr.sigRegionChanged.connect(update_zoom)
-lr.sigRegionChanged.connect(update_plots)
+lr_x.sigRegionChanged.connect(update_zoom)
+lr_x.sigRegionChanged.connect(update_plots)
+lr_y.sigRegionChanged.connect(update_zoom)
+lr_y.sigRegionChanged.connect(update_plots)
 p1.sigXRangeChanged.connect(update_region)
 p1.sigXRangeChanged.connect(update_plots)
 update_zoom()
