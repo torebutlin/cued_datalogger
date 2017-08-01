8e238019517ad633253911bdb7019b0075f91c4c
Theo Brown <tab53@cam.ac.uk>
2017-08-01 10:15:15 +0100

    Code tidy-up

--- circle_fit/circle_fit.py
+++ circle_fit/circle_fit.py
@@ -27,14 +27,9 @@ def from_dB(x):
     return 10**(x/20)
 
 
-def func(w, wr, zr, c):
+def sdof_modal_peak(w, wr, zr, cr, phi):
     """A modal peak"""
-    return c / (wr**2 - w**2 + 2j * zr * wr**2)
-
-
-def func_abs(w, wr, zr, c):
-    """A modal peak"""
-    return np.abs(c / (wr**2 - w**2 + 2j * zr * wr**2))
+    return cr*np.exp(phi) / (wr**2 - w**2 + 2j * zr * wr**2)
 
 
 def circle_fit(data):
@@ -80,15 +75,6 @@ def plot_circle(x0, y0, R0):
     return x, y
 
 
-def find_closest_value(array, value):
-    """Find closest item in array to value"""
-    idx = (np.abs(array-value)).argmin()
-    return array[idx]
-
-
-
-
-
 # Circle Fit window -----------------------------------------------------------
 class CircleFitWidget(QWidget):
     def __init__(self, parent=None):
@@ -96,6 +82,7 @@ class CircleFitWidget(QWidget):
 
         self.init_ui()
 
+    # Initialisation functions ------------------------------------------------
     def init_ui(self):
         # # Transfer function plot
         self.transfer_func_plot_w = pg.PlotWidget(title="Transfer Function",
@@ -108,7 +95,7 @@ class CircleFitWidget(QWidget):
                                                   labels={'bottom': ("Frequency", "rad"),
                                                           'left': ("Transfer Function", "dB")})
         self.region_select_plot = self.region_select_plot_w.getPlotItem()
-        # Region
+        # Region of interest selector
         self.init_roi()
         self.region_select_plot.addItem(self.roi)
 
@@ -126,19 +113,11 @@ class CircleFitWidget(QWidget):
         self.circle_plot.setAspectLocked(lock=True, ratio=1)
         self.circle_plot.showGrid(x=True, y=True)
 
-        # # Fit plot
-        self.fit_plot_w = pg.PlotWidget(title="Fitted peak",
-                                        labels={'bottom': ("Frequency", "rad"),
-                                                'left': ("Transfer Function", "dB")})
-        self.fit_plot = self.fit_plot_w.getPlotItem()
-        self.fit_plot.setMouseEnabled(x=False, y=False)
-
         # # Layout
         plots = QGridLayout()
         plots.addWidget(self.transfer_func_plot_w, 0, 0)
         plots.addWidget(self.region_select_plot_w, 0, 1)
         plots.addWidget(self.circle_plot_w, 2, 1)
-        plots.addWidget(self.fit_plot_w, 2, 0)
         self.setLayout(plots)
 
         self.setWindowTitle('Circle fit')
@@ -156,70 +135,29 @@ class CircleFitWidget(QWidget):
         self.roi.addScaleHandle([1, 1], [0, 0])
         self.roi.addScaleHandle([0, 0], [1, 1])
 
+
+    def set_data(self, w=None, a=None):
+        if a is not None:
+            self.a = a
+        if w is not None:
+            self.w = w
+        else:
+            pass
+        self.transfer_func_plot.plot(x=self.w, y=to_dB(np.abs(self.a)), pen=defaultpen)
+        self.region_select_plot.plot(x=self.w, y=to_dB(np.abs(self.a)), pen=defaultpen)
+        self.update_plots()
+
+    # Update functions --------------------------------------------------------
     def update_zoom(self):
         self.w_min, self.a_min = self.roi.pos()
         self.w_max, self.a_max = self.roi.pos() + self.roi.size()
         self.transfer_func_plot.setXRange(self.w_min, self.w_max, padding=0)
         self.transfer_func_plot.setYRange(self.a_min, self.a_max, padding=0)
-        self.fit_plot.setXRange(self.w_min, self.w_max, padding=0)
-        self.fit_plot.setYRange(self.a_min, self.a_max, padding=0)
 
     def update_roi(self):
         region = np.asarray(self.transfer_func_plot.getViewBox().viewRange())
         self.roi.setPos(region[:, 0])
         self.roi.setSize(region[:, 1] - region[:, 0])
-
-    def update_plots(self):
-        # Get zoomed in region
-        self.get_viewed_region()
-
-        # Clear the current plots
-        self.circle_plot.clear()
-        self.fit_plot.clear()
-        self.circle_plot.getViewBox().removeItem(self.circle_plot.legend)
-        self.circle_plot.addLegend()
-
-        # Update plot
-        # Plot the data
-        self.circle_plot.plot(self.a_reg.real, self.a_reg.imag, pen=None,
-                              symbol='o', symbolPen=None, symbolBrush='r',
-                              name="Data")
-        self.fit_plot.plot(w, np.abs(to_dB(self.a)), pen=defaultpen)
-        # Plot the circle (geometric fit)
-        # Recalculate circle
-        self.x0, self.y0, self.R0 = circle_fit(self.a_reg)
-        # Discard phase information
-        #self.x_c, self.y_c = plot_circle(0, -np.sqrt(self.y0**2 + self.x0**2), self.R0)
-        # Include phase information
-        self.x_c, self.y_c = plot_circle(self.x0, self.y0, self.R0)
-
-        self.circle_plot.plot(self.x_c, self.y_c,
-                              pen=pg.mkPen('k', width=2, style=QtCore.Qt.DashLine))
-
-        """ TEMA METHOD: """
-        w_circle = np.linspace(0, self.w.max(), 1e5)
-        wr, zr, c = self.find_vals()
-        print("TEMA: wr {}, zr {}, c{}".format(wr, zr, c))
-        self.f = func(w_circle, wr, zr, c)
-        self.circle_plot.plot(self.f.real, self.f.imag, pen='g',
-                              name="TEMA method")
-        self.fit_plot.plot(w_circle, np.abs(to_dB(func(w_circle, wr, zr, c))), pen='g')
-
-        """ ITERATIVE METHOD: """
-        wr, zr, c = self.fit_peak()
-        print("Iter: wr {}, zr {}, c{}".format(wr, zr, c))
-        self.f = func(w_circle, wr, zr, c)
-        self.circle_plot.plot(self.f.real, self.f.imag, pen='b',
-                              name="Iterative method")
-        self.fit_plot.plot(w_circle, np.abs(to_dB(func(w_circle, wr, zr, c))), pen='b')
-
-        """JIMS METHOD"""
-        wr, zr, c = self.jim_it()
-        print("Jim: wr {}, zr {}, c{}".format(wr, zr, c))
-        self.f = func(w_circle, wr, zr, c)
-        self.circle_plot.plot(self.f.real, self.f.imag, pen='m',
-                              name="Jims method")
-        self.fit_plot.plot(w_circle, np.abs(to_dB(self.f)), pen='m')
 
     def get_viewed_region(self):
         # Get just the peak we're looking at
@@ -246,132 +184,72 @@ class CircleFitWidget(QWidget):
         self.a_reg = self.a[lower:upper]
         self.w_reg = self.w[lower:upper]
 
-    def fit_peak(self):
+    def update_plots(self):
+        # Get zoomed in region
         self.get_viewed_region()
-        self.a_reg_abs = np.abs(self.a_reg)
 
-        # # Find initial parameters for curve fitting
-        # Find where the peak is
-        i = np.where(self.a_reg_abs == self.a_reg_abs.max())[0][0]
-        wr0 = self.w_reg[i]  # Resonant freq guess
-        c0 = self.a_reg_abs[i]  # Modal constant guess
+        # Clear the current plots
+        self.circle_plot.clear()
+        self.transfer_func_plot.clear()
+        # Delete the old legend and make a new one
+        #self.circle_plot.getViewBox().removeItem(self.circle_plot.legend)
+        #self.circle_plot.addLegend()
 
-        # 3dB points
-        # find the values
-        a3dB = from_dB(to_dB(c0) - 3)
-        a3dB_below = find_closest_value(self.a_reg_abs[:i], a3dB)
-        a3dB_above = find_closest_value(self.a_reg_abs[i:], a3dB)
-        # Find where they are in the array
-        i_a3dB_below = np.where(self.a_reg_abs == a3dB_below)[0][0]
-        i_a3dB_above = np.where(self.a_reg_abs == a3dB_above)[0][0]
+        # Plot the raw data
+        self.circle_plot.plot(self.a_reg.real, self.a_reg.imag, pen=None,
+                              symbol='o', symbolPen=None, symbolBrush='r',
+                              name="Data")
+        self.transfer_func_plot.plot(w, np.abs(to_dB(self.a)), pen=defaultpen)
 
-        z0 = (self.w_reg[i_a3dB_above] - self.w_reg[i_a3dB_below]) / (2 * wr0)  # Damping guess
+        # Plot the circle (geometric fit)
+        # Recalculate circle
+        self.x0, self.y0, self.R0 = circle_fit(self.a_reg)
+        self.x_c, self.y_c = plot_circle(self.x0, self.y0, self.R0)
 
-        popt, pcon = curve_fit(self.modal_peak_abs, self.w_reg, self.a_reg_abs,
-                               p0=[wr0, z0, c0])
-        self.wr = popt[0]
-        self.zr = popt[1]
-        self.c = popt[2]
+        self.circle_plot.plot(self.x_c, self.y_c,
+                              pen=pg.mkPen('k', width=2, style=QtCore.Qt.DashLine))
 
-        return self.wr, self.zr, self.c
+        # Plot the circle we're using to get the values
+        w_circle = np.linspace(0, self.w.max(), 1e5)
+        wr, zr, cr, phi = self.sdof_get_parameters()
+        print("wr={}, zr={}, cr={}, ph={}".format(wr, zr, cr, phi))
+        self.reg_peak = sdof_modal_peak(w_circle, wr, zr, cr, phi)
+        self.circle_plot.plot(self.reg_peak.real, self.reg_peak.imag, pen=pg.mkPen('m', width=1.5),
+                              name="Jim's method")
+        self.transfer_func_plot.plot(w_circle, np.abs(to_dB(self.reg_peak)), pen='m')
 
-    def modal_peak(self, w, wr, zr, c):
-        """A modal peak - complex"""
-        return c / (wr**2 - w**2 + 2j * zr * wr**2)
-
-    def modal_peak_abs(self, w, wr, zr, c):
-        """A modal peak - magnitude"""
-        return np.abs(c / (wr**2 - w**2 + 2j * zr * wr**2))
-
-    def jims_sdof_optimisation_function(self, w, phi, wr, zr, cr):
-        f = self.x0 + 1j*self.y0 - self.R0 * np.exp(1j*phi) + func(w, wr, zr, cr)
+    def sdof_modal_peak_optimisation_function(self, w, phi, wr, zr, cr):
+        f = self.x0 + 1j*self.y0 - self.R0 * np.exp(1j*phi) \
+            - sdof_modal_peak(w, wr, zr, cr, phi)
         return np.real(f * f.conjugate())
 
-    def set_data(self, w=None, a=None):
-        if a is not None:
-            self.a = a
-        if w is not None:
-            self.w = w
-        else:
-            pass
-        self.transfer_func_plot.plot(x=self.w, y=to_dB(np.abs(self.a)), pen=defaultpen)
-        self.region_select_plot.plot(x=self.w, y=to_dB(np.abs(self.a)), pen=defaultpen)
-        self.update_plots()
-
-    def find_vals(self):
-        self.get_viewed_region()
-        self.a_reg_abs = np.abs(self.a_reg)
-
-        # First guess of resonant frequency - the peak
-        i = np.where(self.a_reg_abs == np.abs(self.a_reg.max()))[0][0]
-
-        ## Resonant frequency and phase angle
-        # Four initial data points
-        w0, w1, w2, w3 = self.w_reg[i-2], self.w_reg[i-1], self.w_reg[i+1], self.w_reg[i+2]
-        t0, t1, t2, t3 = np.angle(self.a_reg[i-2]), np.angle(self.a_reg[i-1]), np.angle(self.a_reg[i+1]), np.angle(self.a_reg[i+2])
-
-        t0t1 = self.tAtB(t0, t1, w0, w1)
-        t0t1t2 = self.tAtBtC(t0, t1, t2, w0, w1, w2)
-        t0t1t2t3 = self.tAtBtCtD(t0, t1, t2, t3, w0, w1, w2, w3)
-
-        wr2 = (w0**2 + w1**2 + w2**2 - (t0t1t2 / t0t1t2t3)) / 3
-        wr = np.sqrt(wr2)
-        tr = t0 + (wr2 - w0**2)*t0t1 + (wr2 - w0**2)*(wr2 - w1**2)*t0t1t2 + (wr2 - w0**2)*(wr2 - w1**2)*(wr2 - w2**2)*t0t1t2t3
-
-        ## Damping factor
-        w1, w2, w3, w4, w5, w6 = self.w_reg[i-3], self.w_reg[i-2], self.w_reg[i-1], self.w_reg[i+1], self.w_reg[i+2], self.w_reg[i+3]
-        t1, t2, t3, t4, t5, t6 = np.angle(self.a_reg[i-3]), np.angle(self.a_reg[i-2]), np.angle(self.a_reg[i-1]), np.angle(self.a_reg[i+1]), np.angle(self.a_reg[i+2]), np.angle(self.a_reg[i+3])
-
-        # Take mean of points
-        #3,4
-        zr34 = self.calc_zr(wr, tr, w3, w4, t3, t4)
-
-        #2,5
-        zr25 = self.calc_zr(wr, tr, w2, w5, t2, t5)
-
-        #1,6
-        zr16 = self.calc_zr(wr, tr, w1, w6, t1, t6)
-
-        zr = (zr34 + zr25 + zr16)/3
-
-        ## Modal Constant
-        # Modulus:
-        C = 2*self.R0*wr2*2*zr
-
-        # Phase angle
-        xD = 0
-        yD = 0
-        phi = np.arctan((self.x0 - xD) / (self.y0 - yD))
-
-        return wr, zr, C*np.exp(1j*phi)
-
-    def tAtB(self, tA, tB, wA, wB):
-        return (tA - tB) / (wA**2 - wB**2)
-
-    def tAtBtC(self, tA, tB, tC, wA, wB, wC):
-        return (self.tAtB(tA, tB, wA, wB) - self.tAtB(tB, tC, wB, wC)) / (wA**2 - wC**2)
-
-    def tAtBtCtD(self, tA, tB, tC, tD, wA, wB, wC, wD):
-        return (self.tAtBtC(tA, tB, tC, wA, wB, wC) - self.tAtBtC(tB, tC, tD, wB, wC, wD)) / (wA**2 - wD**2)
-
-    def calc_zr(self, wr, tr, wb, wa, tb, ta):
-        return ((wa**2 - wb**2)/ (wr**2 * (np.tan(ta - tr) + np.tan(tr - tb))))/2
-
-    def jim_it(self):
-        zr0 = 0.02
-        i = np.where(self.a_reg_abs == self.a_reg_abs.max())[0][0]
+    def sdof_get_parameters(self):
+        # # Find initial parameters for curve fitting
+        # Find where the peak is - the maximum magnitude of the amplitude
+        # within the region
+        i = np.where(np.abs(self.a_reg) == np.abs(self.a_reg).max())[0][0]
+        # Take the frequency at the max amplitude as a
+        # first resonant frequency guess
         wr0 = self.w_reg[i]
-        phi0 = 0
-        cr0 = self.a_reg_abs[i]
+        # Take the max amplitude as a first guess for the modal constant
+        cr0 = np.abs(self.a_reg[i])
+        phi0 = np.angle(self.a_reg[i])
+        # First guess of damping factor of 1% (Q of 100)
+        zr0 = 0.01
 
-        popts, pconv = curve_fit(self.jims_sdof_optimisation_function, self.w_reg, np.real(self.a_reg * self.a_reg.conjugate()), [phi0, wr0, zr0, cr0])
+        # # Find the parameter values that give a minimum of
+        # the optimisation function
+        popts, pconv = curve_fit(self.sdof_modal_peak_optimisation_function,
+                                 self.w_reg,
+                                 np.real(self.a_reg * self.a_reg.conjugate()),
+                                 [phi0, wr0, zr0, cr0])
 
         phi = popts[0]
         wr = popts[1]
         zr = popts[2]
         cr = popts[3]
 
-        return wr, zr, cr*np.exp(1j*(phi+np.pi/2))
+        return wr, zr, cr, phi
 
 if __name__ == '__main__':
     app = 0
@@ -381,7 +259,11 @@ if __name__ == '__main__':
 
     # Create a demo transfer function
     w = np.linspace(0, 25, 3e2)
-    a = func(w, 10, 1e-2, 8e12 + 4e6*1j) + func(w, 5, 1e-1, 8e12) + func(w, 20, 5e-2, 8e12) + func(w, 12, 2e-2, -8e12) #+ 5e10*np.random.normal(size=w.size)
+    a = sdof_modal_peak(w, 10, 0.008, 8e12, 0) \
+        + sdof_modal_peak(w, 5, 0.006, 8e12, 0) \
+        + sdof_modal_peak(w, 20, 0.01, 8e12, 0) \
+        + sdof_modal_peak(w, 12, 0.003, -8e12, 0) \
+        #+ 5e10*np.random.normal(size=w.size)
 
     circle_fit_widget.set_data(w, a)
 
