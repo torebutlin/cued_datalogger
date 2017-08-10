a5ae918cc806758d8cdf165bc0a4ad11d1b27d15
En Yi <eyt21@cam.ac.uk>
2017-08-10 14:47:16 +0100

    Experiment

--- datalogger/analysis/analysis_window_testing.py
+++ datalogger/analysis/analysis_window_testing.py
@@ -7,7 +7,7 @@ from PyQt5.QtCore import QCoreApplication, QSize, Qt
 from PyQt5.QtWidgets import (QWidget, QApplication, QTabWidget, QGridLayout, QHBoxLayout,
                              QMainWindow, QPushButton, QMouseEventTransition,
                              QTabBar, QSplitter,QStackedLayout,QLabel, QSizePolicy)
-#from PyQt5.QtGui import QSizePolicy
+from PyQt5.QtGui import QPalette,QColor
 
 from circle_fit import CircleFitWidget
 from sonogram import SonogramWidget
@@ -48,11 +48,19 @@ class collapsibleSideTabs(QWidget):
 
         self.stack_widgets = QWidget(self.splitter)
         self.stack_layout = QStackedLayout(self.stack_widgets)
-        self.stack_layout.addWidget(QLabel('Here',self.stack_widgets))
+        lb1 = QLabel('Here',self.stack_widgets)
+        lb1.setAutoFillBackground(False)
+        self.stack_layout.addWidget(lb1)
         self.stack_layout.addWidget(QLabel('There',self.stack_widgets))
         self.stack_layout.setCurrentIndex(0)
-
-
+        '''
+        pal = QPalette()
+        pal.setColor(QPalette.Background, QColor(255,255,255))
+        pal.setColor(QPalette.Base, QColor(255,255,255))
+        self.stack_widgets.setAutoFillBackground(True)
+        self.stack_widgets.setPalette(pal)
+        '''
+        
         self.tab_bar = QTabBar(self.splitter)
         self.tab_bar.addTab('Tab1')
         self.tab_bar.addTab('Tab2')
@@ -61,31 +69,33 @@ class collapsibleSideTabs(QWidget):
         self.tab_bar.currentChanged.connect(self.change_widget)
         self.tab_bar.tabBarDoubleClicked.connect(self.toggle_collapse)
 
-        self.analysistools_tabwidget = AnalysisTools_TabWidget(self)
-
         self.splitter.addWidget(self.stack_widgets)
         self.splitter.addWidget(self.tab_bar)
-        self.splitter.addWidget(self.analysistools_tabwidget)
 
         self.tab_bar.setSizePolicy(QSizePolicy.Fixed,QSizePolicy.Fixed)
         self.splitter.setStretchFactor(0,0)
         self.splitter.setStretchFactor(1,0)
         self.splitter.setStretchFactor(2,10)
-        self.splitter.setCollapsible(1,False)
+        self.splitter.setChildrenCollapsible(False)
 
         self.UI_layout.addWidget(self.splitter)
+        #"""
+        self.setStyleSheet('''
+                 .QWidget{
+                       background: white;
+                       border: 1px solid #777;
+                       border-right: white;
+                       }
+                 .QSplitter::handle{
+                       background: white;
+                       border-top: 1px solid #777;
+                       border-bottom: 1px solid #777;
+                       }
+                           ''')
 
-        self.splitter.splitterMoved.connect(self.resize_self)
+        #"""
 
-    def resize_self(self, pos,ind):
-        w = min(pos-21,100)
-        if ind == 1:
-            self.splitter.setSizes([w,21,500-21-w])
-        if ind == 2:
-            self.splitter.setSizes([w,21,pos])
-
-    def change_widget(self):
-
+    def change_widget(self,num):
         self.stack_layout.setCurrentIndex(num)
 
     def toggle_collapse(self):
@@ -149,28 +159,34 @@ class AnalysisWindow(QMainWindow):
         self.menubar.addMenu("Addons")
         '''
         # # Create the main widget
-        self.main_widget = QWidget(self)
-        self.main_layout = QHBoxLayout(self.main_widget)
-        #self.main_widget.setLayout(self.main_layout)
         #self.main_widget = QWidget(self)
+        #self.main_layout = QHBoxLayout(self.main_widget)
+        #self.main_widget.setLayout(self.main_layout)
+        self.main_widget = QSplitter(self)
         # Add the sidetabwidget
         try:
             self.sidetabwidget = collapsibleSideTabs(self)
-            self.main_layout.addWidget(self.sidetabwidget)
+            #self.main_layout.addWidget(self.sidetabwidget)
+            self.main_widget.addWidget(self.sidetabwidget)
         except:
             t,v,tb = sys.exc_info()
             print(t)
             print(v)
             print(traceback.format_tb(tb))
-
+        
+        
+        self.sidetabwidget.tab_bar.tabBarDoubleClicked.connect(self.toggle_collapse)
 
         # Add the analysis tools tab widget
-        #self.analysistools_tabwidget = AnalysisTools_TabWidget(self)
-        #self.main_widget.addWidget(self.analysistools_tabwidget)
+        self.analysistools_tabwidget = AnalysisTools_TabWidget(self)
+        self.main_widget.addWidget(self.analysistools_tabwidget)
 
         #self.main_widget.setChildrenCollapsible(False)
+        self.main_widget.setChildrenCollapsible(False)
         self.setCentralWidget(self.main_widget)
-
+        
+    def toggle_collapse(self):
+        self.main_widget.setSizes([w.sizeHint().width() for w in [self.sidetabwidget,self.analysistools_tabwidget]])
 
 if __name__ == '__main__':
     app = 0
