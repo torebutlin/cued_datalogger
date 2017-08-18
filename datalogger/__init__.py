56db40e4718b6e39b5e2e8475bef59f7d60c5cb8
Theo Brown <tab53@cam.ac.uk>
2017-08-18 08:31:28 +0100

    Add imports of all bin modules

---
+++ datalogger/__init__.py
@@ -0,0 +1,7 @@
+from .bin import (addons, channel, custom_plot, DataAnalysisPlot, file_import,
+                 numpy_functions, pyqt_widgets, workspace)
+
+from . import analysis_window_testing
+from . import liveplotUI
+from . import analysis
+from . import acquisition
--- setup.py
+++ setup.py
@@ -61,29 +61,28 @@ else:
 
 
 setup(name='CUED_DataLogger',
-      version='0.0.24',
+      version='0.0.28',
       description='The CUED DataLogger for acquiring and analysing data',
       long_description=readme(),
       url='https://bitbucket.org/tab53/cued_datalogger/',
       author='Theo Brown, En Yi Tee',
       author_email='tab53@cam.ac.uk, eyt21@cam.ac.uk',
       license='BSD 3-Clause License',
-      packages=['datalogger',
-                'datalogger/acquisition',
-                'datalogger/analysis',
-                'datalogger/bin'],
+      packages=['datalogger'],
+                #'datalogger/acquisition',
+                #'datalogger/analysis'],
       # TODO
       # If you include this in the setup code, when it tries to install
       # itself in an Anaconda environment, a lot of things break. Fix this.
       # (currently the workaround is telling the user to install some stuff)
-      #install_requires=[
-      #                'PyQt5>=5.9',
-      #                'numpy>=1.11.3',
-      #                'scipy>=0.18.1',
-      #                'pyqtgraph>=0.9.10',
-      #                'matplotlib>=1.5.1',
-      #                'PyDAQmx>=1.3.2',
-      #                'pyaudio>=0.2.11'],
+      install_requires=[
+                      'PyQt5>=5.9',
+                      'numpy>=1.11.3',
+                      'scipy>=0.18.1',
+                      'pyqtgraph>=0.9.10',
+                      'matplotlib>=1.5.1',
+                      'PyDAQmx>=1.3.2',
+                      'pyaudio>=0.2.11'],
       entry_points={
         'console_scripts': ['DataLogger_cmd = datalogger.__main__:full'],
         'gui_scripts': ['DataLogger = datalogger.__main__:full']},
