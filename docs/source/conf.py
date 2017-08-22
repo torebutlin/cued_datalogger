3ef8cb44cf5d210cf9ef0bc5de7a7e0401dac0fd
Theo Brown <tab53@cam.ac.uk>
2017-08-22 16:00:00 +0100

    Build docs, try excluding 'datalogger'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -158,7 +158,7 @@ texinfo_documents = [
 ]
 
 #-----------Options put in by Theo--------------------------------------------
-autodoc_mock_imports = ["pyaudio"]
+autodoc_mock_imports = ["datalogger"]
 
 
 
