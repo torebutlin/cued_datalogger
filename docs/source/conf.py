4db5f729350dcb7f59abee22604e7f37177d4fff
Theo Brown <tab53@cam.ac.uk>
2017-08-22 16:32:21 +0100

    Mock scipy

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -168,7 +168,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['pyaudio']
+MOCK_MODULES = ['pyaudio', 'scipy']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
