26f2cdc6963b92adab23874b278bd632fcf2d436
Theo Brown <tab53@cam.ac.uk>
2017-08-22 16:13:30 +0100

    Build again, this time with manual mocking

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -158,7 +158,18 @@ texinfo_documents = [
 ]
 
 #-----------Options put in by Theo--------------------------------------------
-autodoc_mock_imports = ["datalogger"]
+#autodoc_mock_imports = ["datalogger", "pyaudio"]
+
+import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['pyaudio']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
 
