b8f4b8dd872d357c52f8ee8c0630b6e55151b6cc
Theo Brown <tab53@cam.ac.uk>
2017-08-23 13:49:40 +0100

    Relativise VERSION path

--- datalogger/__init__.py
+++ datalogger/__init__.py
@@ -3,13 +3,15 @@ import analysis
 import acquisition
 import analysis_window, acquisition_window
 
-from datalogger.api import workspace as workspace
+#from datalogger.api import workspace as workspace
+from api import workspace as workspace
 
 import os.path
 
-_PKG_ROOT = os.path.abspath(os.path.dirname(__file__))
+#_PKG_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(_PKG_ROOT, 'VERSION')) as version_file:
+#with open(os.path.join(_PKG_ROOT, 'VERSION')) as version_file:
+with open('VERSION') as version_file:
     __version__ = version_file.read()
 
 # Conceal modules that we don't want the user to see by deleting the 
