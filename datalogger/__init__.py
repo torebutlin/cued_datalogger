54d19bbb17b7d429f959b70f942fe04fb98f2fe4
Theo Brown <tab53@cam.ac.uk>
2017-08-23 14:19:57 +0100

    Attempt to include VERSION

--- datalogger/__init__.py
+++ datalogger/__init__.py
@@ -9,7 +9,7 @@ import os.path as _path
 
 _PKG_ROOT = _path.abspath(_path.dirname(__file__))
 
-with open(_path.join(_PKG_ROOT, 'VERSION')) as _version_file:
+with open(_path.join(_PKG_ROOT, 'VERSION.txt')) as _version_file:
 #with open('VERSION') as _version_file:
     __version__ = _version_file.read()
 
