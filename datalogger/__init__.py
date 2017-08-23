0aae3855c6f1222464809856419df86f180025a2
Theo Brown <tab53@cam.ac.uk>
2017-08-23 13:54:42 +0100

    Hack __init__

--- datalogger/__init__.py
+++ datalogger/__init__.py
@@ -11,12 +11,14 @@ import os.path
 #_PKG_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 #with open(os.path.join(_PKG_ROOT, 'VERSION')) as version_file:
+"""
 with open('VERSION') as version_file:
     __version__ = version_file.read()
+"""
 
 # Conceal modules that we don't want the user to see by deleting the 
 # local references
 del os
-del version_file
+#del version_file
 
     
